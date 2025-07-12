f7cd4b6fc289cdf494942e7249f719ab260fb7ba
Victor Hugo <csivictorhugo@gmail.com>
2025-07-12 18:02:51 -0300

    smooth scroll navigation

--- script.js
+++ script.js
@@ -39,3 +39,30 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+function initSmoothScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    //  forma alternativa
+    // const top = section.offsetTop;
+    // window.scrollTo({
+    //   top: top,
+    //   behavior: 'smooth',
+    // })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initSmoothScroll();
