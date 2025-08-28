17904c9de5f73104fe19fdf017d130fa1abf23bd
Victor Hugo <csivictorhugo@gmail.com>
2025-08-28 18:27:24 -0300

    remove console.log

--- js/script.js
+++ js/script.js
@@ -42,7 +42,6 @@ const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']");
 menuMobile.init();
 
 const funcionamento = new Funcionamento("[data-semana]", 'aberto');
-console.log(funcionamento);
 funcionamento.init();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
