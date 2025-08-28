f2fd44e469f0ddca5c2c21a87d882482cb84ebf3
Victor Hugo <csivictorhugo@gmail.com>
2025-08-28 18:28:42 -0300

    missed build

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ dropdownMenu.init();
 const menuMobile = new MenuMobile("[data-menu='button']", "[data-menu='list']");
 menuMobile.init();
 
-const funcionamento = new Funcionamento("[data-semana]", 'aberto');
+const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
 
 fetchAnimais("../../animaisapi.json", ".numeros-grid");
